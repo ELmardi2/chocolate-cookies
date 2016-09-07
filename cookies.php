894fa1c8ca3d7917d6a9bb92455307f465f438be
Elmardi2 <mardi.yahia@gmail.com>
2016-09-07 16:43:44 +0200

    set cookie

--- cookies.php
+++ cookies.php
@@ -1,15 +1,26 @@
 <?php
-$cookie_name = "user";
-$cookie_value = "mardino rossi";
-setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
 
-
-
-
-if(!isset($_COOKIE[$cookie_name])) {
-    echo "Cookie named '" . $cookie_name . "' is not set!";
-} else {
-    echo "Cookie '" . $cookie_name . "' is set!<br>";
-    echo "Value is: " . $_COOKIE[$cookie_name];
+setcookie('mardi', 'test', time() + (86400 * 30), "/"); // 86400 = 1 day
+if (count($_COOKIE) > 0) {
+  echo "ouiiiii";
+}else {
+  echo "non set SVP";
 }
 ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+if(!isset($_COOKIE['mardi'])) {
+    echo "Cookie named '" . 'mardi' . "' is not set!";
+} else {
+    echo "Cookie '" . 'mardi' . "' is set!<br>";
+    echo "Value is: " . $_COOKIE['mardi'];
+}
+?>
+  </body>
+</html>
